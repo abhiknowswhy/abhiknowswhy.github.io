ffa2313fe0a1d9092fae561a5873bed0c86d40d8
Abhiram Bondada <abondada@microsoft.com>
2025-08-25 23:10:54 +0530

    added more pages

--- src/config/pageComponents.ts
+++ src/config/pageComponents.ts
@@ -1,10 +1,17 @@
 import React from 'react';
 const Home = React.lazy(() => import('../pages/Home/Home'));
 const About = React.lazy(() => import('../pages/About/About'));
+
+const Work = React.lazy(() => import('../pages/Work/Work'));
+const Blogs = React.lazy(() => import('../pages/Blogs/Blogs'));
+const Connect = React.lazy(() => import('../pages/Connect/Connect'));
 const NotFound = React.lazy(() => import('../pages/NotFound/NotFound'));
 
 export const pageComponents: Record<string, React.LazyExoticComponent<React.FC>> = {
   Home,
   About,
+  Work,
+  Blogs,
+  Connect,
   NotFound,
 };
